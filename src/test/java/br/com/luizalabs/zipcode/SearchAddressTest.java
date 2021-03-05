39ad237bd46640ae23e1ef25cd9167fedb2675a2
Kaike <kaike>
2021-03-05 17:36:54 -0300

    Optimization of class imports.

--- src/test/java/br/com/luizalabs/zipcode/SearchAddressTest.java
+++ src/test/java/br/com/luizalabs/zipcode/SearchAddressTest.java
@@ -11,17 +11,13 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import static org.assertj.core.api.Assertions.assertThat;
-
 
 import java.util.Optional;
 import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
